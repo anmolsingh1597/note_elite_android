6dd76437f351b76bd23071317c5f8014aeb3f7f5
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-12 22:40:01 -0400

    [Anmol Singh]NotesCardViewActivity: Layout Designed

---
+++ app/src/main/java/com/lambton/note_elite_android/model/FolderNoteRelation.java
@@ -0,0 +1,43 @@
+package com.lambton.note_elite_android.model;
+
+
+import com.raizlabs.android.dbflow.annotation.ForeignKey;
+import com.raizlabs.android.dbflow.annotation.ForeignKeyAction;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import com.lambton.note_elite_android.database.AppDatabase;
+
+/**
+ * Created by MohMah on 8/17/2016.
+ */
+@Table(database = AppDatabase.class, allFields = true)
+public class FolderNoteRelation extends BaseModel {
+	private static final String TAG = "FolderNoteRelation";
+
+	@PrimaryKey
+	@ForeignKey(onDelete = ForeignKeyAction.CASCADE)
+	private Folder folder;
+
+	@PrimaryKey
+	@ForeignKey(onDelete = ForeignKeyAction.CASCADE)
+	private Note note;
+
+	public Folder getFolder(){
+		return folder;
+	}
+
+	public void setFolder(Folder folder){
+		this.folder = folder;
+	}
+
+	public Note getNote(){
+		return note;
+	}
+
+	public void setNote(Note note){
+		this.note = note;
+	}
+
+}
