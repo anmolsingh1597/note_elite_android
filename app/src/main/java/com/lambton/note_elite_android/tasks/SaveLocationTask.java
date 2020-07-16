d667decf3d04ec3fba2efdcf7765f24f4c6e4ab7
Aman Kaur <amankaur2628@gmail.com>
2020-07-16 15:31:30 -0400

    [Aman] Location saved to text file for reference

--- app/src/main/java/com/lambton/note_elite_android/tasks/SaveLocationTask.java
+++ app/src/main/java/com/lambton/note_elite_android/tasks/SaveLocationTask.java
@@ -1,10 +1,7 @@
 package com.lambton.note_elite_android.tasks;
-
 import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.birbit.android.jobqueue.Job;
 import com.birbit.android.jobqueue.Params;
 import com.birbit.android.jobqueue.RetryConstraint;
@@ -12,42 +9,34 @@ import com.google.android.gms.maps.model.LatLng;
 import com.lambton.note_elite_android.actions.NoteEditedEvent;
 import com.lambton.note_elite_android.database.NotesDAO;
 import com.lambton.note_elite_android.model.Note;
-
 import org.greenrobot.eventbus.EventBus;
-
 import java.util.Locale;
 
 public class SaveLocationTask extends Job {
-
     private static final String TAG = "SaveLocationTask";
     private final int noteId;
     private final LatLng notesLocation;
-
     public SaveLocationTask(LatLng notesLocation, int noteId){
         super(new Params(1));
         this.notesLocation = notesLocation;
         this.noteId = noteId;
-
     }
     @Override
     public void onAdded() {
         Log.e(TAG, "onAdded() called with: " + "");
     }
-
     @Override
     public void onRun() throws Throwable {
         Log.e(TAG, "onRun() called with: " + "");
         Note note = NotesDAO.getNote(noteId);
+//        String noteSubString = note.getBody();
         note.setBody(note.getBody() + "Location: "+ String.format(Locale.CANADA, "%.4f", (notesLocation.latitude)) + "," + String.format(Locale.CANADA, "%.4f", (notesLocation.longitude)));
         note.save();
         EventBus.getDefault().post(new NoteEditedEvent(note.getId()));
     }
-
     @Override
     protected void onCancel(int cancelReason, @Nullable Throwable throwable) {
-
     }
-
     @Override
     protected RetryConstraint shouldReRunOnThrowable(@NonNull Throwable throwable, int runCount, int maxRunCount) {
         return null;
